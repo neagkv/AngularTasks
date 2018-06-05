7d4246142540e6b0a85ce904c54f81818c63bb18
Kevin Neag <neagkv@gmail.com>
2018-06-05 15:05:28 -0400

    angular added

--- src/main/java/com/angular/tasks/domain/Task.java
+++ src/main/java/com/angular/tasks/domain/Task.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
